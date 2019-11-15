6b6294719de9362c67c1852403c85d31423a4b0c
seaung <seaung.c0m7de@protonmail.com>
2019-11-16 01:08:11 +0800

    crawler version 1.0

---
+++ crawler_sync.py
@@ -0,0 +1,49 @@
+import urllib.request
+import urllib.error
+import urllib.parse
+import re
+
+
+patterns_a_href_link = re.compile(r'''<a href="(.*?)"''')
+
+
+def parser_link(response):
+    """parse link from http response content
+    :param response: http response content
+    :return:
+    """
+    a_links = patterns_a_href_link.findall(response)
+
+    if a_links:
+        for link in a_links:
+            url_parser = urllib.parse.urlparse(link)
+            if url_parser.scheme == "javascript" or url_parser.scheme == "" and url_parser.netloc == "":
+                continue
+            if not link.startswith("http://") and not link.startswith("https://"):
+                link = "http://" + url_parser.netloc + url_parser.path + url_parser.params + url_parser.query + url_parser.fragment
+            print(link)
+
+
+
+def downloader(url):
+    """download request content
+    :param url: target website url
+    :param headers: http request headers
+    :return: http response content
+    """
+    try:
+        request = urllib.request.Request(url=url)
+        request.add_header("User-Agent", "__spider_man__")
+        response = urllib.request.urlopen(request)
+        if response.getcode() == 200:
+            return response.read().decode("utf-8")
+    except (urllib.error.URLError, urllib.error.HTTPError) as e:
+        pass
+
+
+if __name__ == "__main__":
+    url = str(input("enter a url address"))
+    response = downloader(url)
+
+    parser_link(response)
+
